cefe5077d452fbe08affe0a6afb0337dc3b18024
audahadi <wan.audahadi@gmail.com>
2013-12-28 23:24:07 +0800

    update sense build script

--- sense.sh
+++ sense.sh
@@ -20,9 +20,9 @@ make -C drivers/net/wireless/compat-wireless_R5.SP2.03 KLIB=`pwd` KLIB_BUILD=`pw
 wait
 cp arch/arm/boot/zImage ../next_sense/zImage
 wait
-find . -name "*.ko" -exec cp {} ../next_sense \;
+find . -name "*.ko" -exec cp {} ../next_sense/modules \;
 wait
-cp -f drivers/net/wireless/compat-wireless_R5.SP2.03/net/wireless/cfg80211.ko ../next_sense
+cp -f drivers/net/wireless/compat-wireless_R5.SP2.03/net/wireless/cfg80211.ko ../next_sense/modules
 endBuildTimeM=$(date +%r)
 
 echo " "
